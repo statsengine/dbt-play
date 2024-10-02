4154d296885e03a8a4a5d0921435325e6a575027
Adam <adam@signific.se>
2024-10-02 17:59:59 +0200

    Fix

--- models/intermediate/core/int_all_events.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized="view") }}
-
-with sessions as (select * from {{ ref("stg_google_analytics_events") }})
-
-select *
-from sessions
--- tests/assert_user_id_not_null.sql
+++
@@ -1,3 +0,0 @@
-select countif(user_pseudo_id is null) as n_null,
-from {{ ref("int_all_events") }}
-having n_null > 0
